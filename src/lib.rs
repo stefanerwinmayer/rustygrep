f9e8251e2766fe0de14fb7ba67ec097ecba2cc0d
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2019-02-21 18:04:19 +0000

    Add case insensitive search
    Signed-off-by: Stefan E. Mayer <stefan.e.mayer@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -14,14 +16,26 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -40,18 +54,47 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "truth";
         let contents = "\
+Truth is:
 When you have eliminated all which is impossible,
 then whatever remains, however improbable,
 must be the truth.";
 
         assert_eq!(vec!["must be the truth."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "trUth";
+        let contents = "\
+Truth is:
+When you have eliminated all which is impossible,
+then whatever remains, however improbable,
+must be the truth.";
+
+        assert_eq!(
+            vec!["Truth is:", "must be the truth."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
